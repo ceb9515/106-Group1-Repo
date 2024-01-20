197e4ac8558e4c1f789f331d577a24a3608c8c74
JakeShapiro1 <jts3681@rit.edu>
2024-01-19 19:00:21 -0500

    Added IsPalindrome method

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -57,6 +57,26 @@ namespace Test
             //return the result
             return firstName + lastName;
         }
+
+        /// <summary>
+        /// determines if a word is a palindrome
+        /// </summary>
+        /// <param name="word">incoming word to be checked</param>
+        /// <returns>true if palindrome, false otherwise</returns>
+        public static bool IsPalindrome(string word)
+        {
+            int count = word.Length - 1;
+            word = word.ToLower();
+            for (int x = 0; x < word.Length; x++)
+            {
+                if (word[x] != word[count])
+                {
+                    return false;
+                }
+                count--;
+            }
+            return true;
+        }
     }
 
 }
