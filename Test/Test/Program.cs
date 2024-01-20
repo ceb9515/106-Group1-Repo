87b21798363c95a6bdfd373412b49d36068765b0
ceb9515 <143808659+ceb9515@users.noreply.github.com>
2024-01-20 17:13:44 -0500

    Completed DiceSum task, merging with main

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -16,9 +16,44 @@ namespace Test
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DiceSum();
         }
 
+
+        /// <summary>
+        /// User enters sum for the program to roll for
+        /// </summary>
+        public static void DiceSum()
+        {
+            int sumToRoll = 0;
+            Random rng = new Random();
+
+            int rollOne = 0;
+            int rollTwo = 0;
+
+            bool desireCheck = false;
+            while (desireCheck == false)
+            {
+                Console.Write("Desired dice sum: ");
+                sumToRoll = int.Parse(Console.ReadLine()!.Trim());
+                if (sumToRoll >= 2 & sumToRoll <= 12)
+                {
+                    desireCheck = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid sum\n");
+                }
+            }
+
+            while (rollOne + rollTwo != sumToRoll)
+            {
+                rollOne = rng.Next(1, 7);
+                rollTwo = rng.Next(1, 7);
+                Console.WriteLine(rollOne + " and " + rollTwo + " = " + (rollOne + rollTwo));
+            }
+
+        }
     }
 
 }
