65bb407cebc9d543e283577ce5bbcce1c8ecd56b
CShoffler <cms6711@rit.edu>
2024-01-23 22:41:41 -0500

    Implemented Data Processor Class + Example Implementation

---
+++ HW1 - Story Generator/DataProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1___Story_Generator
+{
+    internal class DataProcessor
+    {
+        /// <summary>
+        /// Converts a text file into a nested list of useable strings from each line of the file.
+        /// </summary>
+        /// <param name="filename">file to be read</param>
+        /// <returns>list of data lines formatted as strings</returns>
+        public List<string[]> Load(string filename)
+        {
+            //setup stream reader to grab input from file + establish nested list
+            StreamReader input = null;
+            input = new StreamReader(filename);
+            List<string[]> dataList = new List<string[]>();
+
+            //loop for reading each line
+            string line = "";
+            while ((line = input.ReadLine()) != null)
+            {
+                //split the line into strings
+                string[] splitLine = line.Split(",");
+                dataList.Add(splitLine);
+            }
+            //return the finalized list
+            return dataList;
+        }
+    }
+}
---
+++ HW1 - Story Generator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HW1___Story_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a DataProcessor and read your file (change "testData" to the name of your file"
+            DataProcessor dataProcessor = new DataProcessor();
+            List<string[]> storyData = dataProcessor.Load("../../../testData.txt");
+            //loop through string arrays in list
+            for(int i = 0; i < storyData.Count(); i++)
+            {
+                //loop through strings in array
+                for(int k = 0; k < storyData[i].Length; k++)
+                {
+                    //write data point with a space inbetween each
+                    Console.Write(storyData[i][k] + " ");
+                }
+                //add line break
+                Console.WriteLine();
+            }
+        }
+    }
+}
