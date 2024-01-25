b29d0035719ecfbdaa2cfc59e515c9af5e06ba1d
ceb9515 <143808659+ceb9515@users.noreply.github.com>
2024-01-24 23:36:27 -0500

    Finished Conclusion part

---
+++ HW1 - Story Generator/Conclusion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1___Story_Generator
+{
+    /// <summary>
+    /// Uses data processor to process conclusion file, 
+    /// then select a conclusion based on the chosen genre
+    /// </summary>
+    internal class Conclusion
+    {
+        Random rng;
+
+        //List generated from data file
+        List<string[]> conclusionGenerator;
+
+        //Data processor
+        DataProcessor dataProcessor;
+
+        //List to fill with endings to be randomly selected
+        List<string> appropriateConclusions;
+
+        public Conclusion()
+        {
+            dataProcessor = new DataProcessor();
+            conclusionGenerator = dataProcessor.Load("../../../Conclusions.txt");
+            appropriateConclusions = new List<string>();
+            rng = new Random();
+        }
+
+        /// <summary>
+        /// filters the conclusion genres, and selects one
+        /// </summary>
+        /// <param name="choice"> type of ending requested </param>
+        /// <returns> an ending </returns>
+        public string Sort(string choice)
+        {
+            appropriateConclusions.Clear();
+            for (int i = 0; i < conclusionGenerator.Count; i++)
+            {
+                if (conclusionGenerator[i][0] == choice)
+                {
+                    appropriateConclusions.Add(conclusionGenerator[i][1]);
+                }
+            }
+
+            return appropriateConclusions[rng.Next(0, appropriateConclusions.Count())];
+        }
+
+        /// <summary>
+        /// Adds all endings to the list, then randomly selects one
+        /// </summary>
+        /// <returns> one ending </returns>
+        public string AnyEnding()
+        {
+            appropriateConclusions.Clear();
+            for (int i = 0; i < conclusionGenerator.Count; i++)
+            {
+                appropriateConclusions.Add(conclusionGenerator[i][1]);
+            }
+
+            return appropriateConclusions[rng.Next(0, appropriateConclusions.Count())];
+        }
+    }
+}
--- HW1 - Story Generator/Program.cs
+++ HW1 - Story Generator/Program.cs
@@ -4,6 +4,72 @@
     {
         static void Main(string[] args)
         {
+            string story = "";
+            Conclusion conclusion = new Conclusion();
+            Console.WriteLine("What kind of conclusion do you want? \n Tragic \t " +
+                "Happy \t Comedic \n Cliffhanger \t Realistic \t Any");
+            string userChoice = Console.ReadLine()!.Trim().ToLower();
+            bool choosing = true;
+            while (choosing == true)
+            {
+                switch (userChoice)
+                {
+                    case "tragic":
+                        {
+                            story = conclusion.Sort(userChoice);
+                            choosing = false;
+                            break;
+                        }
+
+                    case "happy":
+                        {
+                            story = conclusion.Sort(userChoice);
+                            choosing = false;
+                            break;
+                        }
+
+                    case "comedic":
+                        {
+                            story = conclusion.Sort(userChoice);
+                            choosing = false;
+                            break;
+                        }
+
+                    case "cliffhanger":
+                        {
+                            story = conclusion.Sort(userChoice);
+                            choosing = false;
+                            break;
+                        }
+
+                    case "realistic":
+                        {
+                            story = conclusion.Sort(userChoice);
+                            choosing = false;
+                            break;
+                        }
+
+                    case "any":
+                        {
+                            story = conclusion.AnyEnding();
+                            choosing = false;
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Invalid genre.");
+                            break;
+                        }
+                }
+
+                Console.WriteLine(story);
+            }
+
+
+
+
+
             //create a DataProcessor and read your file (change "testData" to the name of your file"
             DataProcessor dataProcessor = new DataProcessor();
             List<string[]> storyData = dataProcessor.Load("../../../testData.txt");
