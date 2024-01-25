ccc30719294ac18a549f85f843c05212bc23cbba
JakeShapiro1 <jts3681@rit.edu>
2024-01-25 15:06:53 -0500

    created setting class

---
+++ HW1 - Story Generator/Setting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1___Story_Generator
+{
+    internal class Setting : DataProcessor
+    {
+        // fields
+        private Random rand;
+        private List<string[]> settings;
+
+        // constructor
+
+        /// <summary>
+        /// loads a list of all the settings
+        /// </summary>
+        public Setting()
+        {
+            settings = Load("../../../Setting.txt");
+            rand = new Random();
+        }
+
+        // methods
+
+        /// <summary>
+        /// gets a random setting from the list of settings
+        /// </summary>
+        /// <returns>returns a random setting that holds location and time of day</returns>
+        public string[] GetASetting()
+        {
+            int randomSetting = rand.Next(0, 5);
+            return settings[randomSetting];
+        }
+    }
+}
