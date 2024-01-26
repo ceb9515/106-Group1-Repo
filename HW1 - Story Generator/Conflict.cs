ebe3aab085d89e8b748b09e2ac2567520c22c06e
xsu217822 <143617128+xsu217822@users.noreply.github.com>
2024-01-26 17:18:05 -0500

    conflict

---
+++ HW1 - Story Generator/Conflict.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1___Story_Generator
+{
+    internal class Conflict : DataProcessor
+    {
+        //fields
+        private Random rng = new Random();
+        private List<string[]> conflicts;
+
+        public Conflict()
+        {
+            conflicts = Load("../../../conflictClass.txt");
+        }
+
+        public string[] GetConflict(List<string[]> conflicts)
+        {
+            int number = conflicts.Count;
+            return conflicts[rng.Next(number)];
+        }
+
+
+
+    }
+}
