9f66dc1361a7cd8c6d38f3919da8de8a8a46977b
CShoffler <cms6711@rit.edu>
2024-03-20 13:34:30 -0400

    Added Support for WindowsForms

--- Dualing Drivers/Project1/Program.cs
+++ Dualing Drivers/Project1/Program.cs
@@ -1,3 +1,14 @@
 ﻿
-using var game = new Project1.Game1();
-game.Run();
+namespace Project1
+{
+    class Program
+    {
+        [System.STAThread]
+        static void Main(string[] args)
+        {
+            using var game = new Project1.Game1();
+            game.Run();
+        }
+    }
+}
+
