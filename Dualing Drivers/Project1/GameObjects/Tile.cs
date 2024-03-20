1a76cea642213bb06b72f7b1e808111b4d8a8db1
JakeShapiro1 <jts3681@rit.edu>
2024-03-20 16:48:27 -0400

    added to tile class

--- Dualing Drivers/Project1/GameObjects/Tile.cs
+++ Dualing Drivers/Project1/GameObjects/Tile.cs
@@ -7,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace Project1
 {
+    enum TileType
+    {
+        solid,
+        breakable,
+        semiSolid,
+        background
+    }
     internal class Tile : GameObject
     {
+        // fields
+        private int health;
+        private TileType tileType;
+
+        // constructor
+
         /// <summary>
         /// Basic Tile Constructor
         /// </summary>
@@ -17,9 +30,36 @@ namespace Project1
         /// <param name="y">y value of position rectangle</param>
         /// <param name="width">width of position rectangle</param>
         /// <param name="height">height of position rectangle</param>
-        public Tile(Texture2D texture, int x, int y, int width, int height) : base(texture, x, y, width, height)
+        public Tile(Texture2D texture, int x, int y, int width, int height, TileType tileType) : base(texture, x, y, width, height)
         {
+            if (tileType == TileType.breakable)
+            {
+                health = 9;
+            }
 
+            this.tileType = tileType;
+        }
+
+        // methods
+
+        /// <summary>
+        /// breakable tiles take damage and destroy after three hits
+        /// </summary>
+        public void TakeDamage()
+        {
+            if (tileType == TileType.breakable)
+            {
+                health -= 3;
+                if (health <= 0)
+                {
+                    Destroy();
+                }
+            }
+        }
+
+        public void BlockPlayer()
+        {
+            
         }
     }
 }
