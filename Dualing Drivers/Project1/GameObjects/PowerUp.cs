28f50bdb466be7457f16ba83fe42d37587ca3cab
JakeShapiro1 <jts3681@rit.edu>
2024-04-21 13:49:11 -0400

    added to power up

--- Dualing Drivers/Project1/GameObjects/PowerUp.cs
+++ Dualing Drivers/Project1/GameObjects/PowerUp.cs
@@ -10,7 +10,26 @@ namespace Project1
 {
     internal class PowerUp : GameObject
     {
+        public enum PowerUpType
+        {
+            health,
+            speed,
+            ammo
+        }
+
+        // fields
+        private PowerUpType type;
+        private bool active;
+
+        // properties
         
+        /// <summary>
+        /// gets active status of power up
+        /// </summary>
+        public bool Active { get { return active; } }
+
+        // constructor
+
         /// <summary>
         /// Basic PowerUp Constructor
         /// </summary>
@@ -19,9 +38,38 @@ namespace Project1
         /// <param name="y">y value of position rectangle</param>
         /// <param name="width">width of position rectangle</param>
         /// <param name="height">height of position rectangle</param>
-        public PowerUp(Texture2D texture, int x, int y, int width, int height) : base(texture, x, y, width, height)
+        public PowerUp(Texture2D texture, int x, int y, int width, int height, PowerUpType type) 
+            : base(texture, x, y, width, height)
         {
-            
+            this.type = type;
+            active = true;
+        }
+
+        // methods
+
+        /// <summary>
+        /// power ups the player based on power up type
+        /// </summary>
+        /// <param name="player">player who picked up power up</param>
+        public void PowerUpPlayer(Player player)
+        {
+            if (player.PlayerRect.Intersects(this.rect))
+            {
+                if (this.type == PowerUpType.health)
+                {
+                    // gives player health
+                    player.Health += 3;
+                }
+                else if (this.type == PowerUpType.ammo)
+                {
+                    // increase max ammo
+                }
+                else if (this.type == PowerUpType.speed)
+                {
+                    // increase player speed
+                    player.Speed += 1;
+                }
+            }
         }
        
     }
